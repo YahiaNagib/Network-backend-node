4c9eb48c3f43950822645722840ede4210b7179f
YahiaNagib <yahianagib123@gmail.com>
2021-01-27 02:32:50 +0200

    add single user posts and followed users posts routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,44 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const { User } = require("../models/users")
 const { Post } = require("../models/posts")
 const router = express.Router();
 
+// get all the posts
 router.get('/', async (req, res) => {
     const posts = await Post.find();
+    res.send(posts);
+});
+
+// get all the posts of a certain user
+router.get('/user/:id', async (req, res) => {
+    const { id: userId } = req.params;
+    const posts = await Post.find({ owner: userId });
+    res.send(posts);
+});
+
+// get all the posts of users who a certain user follows
+router.get('/following/:id', async (req, res) => {
+    const { id: userId } = req.params;
+    // first get all the followed users 
+    const { following: followedUsers } = await User.findOne({ _id: userId });
+
+    let posts = [];
+    // then get the posts of each user and add it to the array
+
+    // followedUsers.forEach(async (user) => {
+    //     const userPosts = await Post.find({ owner: mongoose.Types.ObjectId(user) });
+    //     posts.push(...userPosts);
+    //     console.log(posts);
+    // });
+
+    // use regular for loop instead of foreach (Asynchronous/Promise issue)
+    // https://stackoverflow.com/questions/37576685/using-async-await-with-a-foreach-loop
+    for (let user of followedUsers) {
+        const userPosts = await Post.find({ owner: mongoose.Types.ObjectId(user) });
+        posts.push(...userPosts);
+    }
+
     res.send(posts);
 });
 
@@ -25,8 +59,9 @@ router.post('/', async (req, res) => {
 
 });
 
-router.put('/', async (req, res) => {
-    const { id: postId, content } = req.body;
+router.put('/:id', async (req, res) => {
+    const { id: postId } = req.params
+    const { content } = req.body;
     let post = await Post.findById(postId);
     if (!post) return res.status(400).send('This post does not exist!');
     post = await Post.findOneAndUpdate(
@@ -36,12 +71,12 @@ router.put('/', async (req, res) => {
     res.send(post);
 });
 
-router.delete('/', async (req, res) => {
-    const postId = req.body.id;
+router.delete('/:id', async (req, res) => {
+    const postId = req.params.id;
     let post = await Post.findById(postId);
     if (!post) return res.status(400).send('This post does not exist!');
 
-    post = await Post.deleteOne({_id: postId});
+    post = await Post.deleteOne({ _id: postId });
     res.send(post);
 });
 
